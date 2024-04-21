415348d417aefccf6a4d22ccbf407573204fe288
AndFran <andrewwilliamfranks@gmail.com>
2024-04-21 21:08:16 +0200

    Create main.go
    implements the simple pattern where we can have a variable number of workers who communicate when the work is finished so the main goroutine / calling goroutine can shut down correctly.

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func worker(workChannel chan int, result chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		i, ok := <-workChannel
+		// fake long processes
+		time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+		if !ok {
+			return
+		}
+		i *= i
+		result <- i
+	}
+}
+
+func producer(workChannel chan int) {
+	for i := 0; i < 400; i++ {
+		workChannel <- i
+	}
+	close(workChannel)
+}
+
+func main() {
+	workChannel := make(chan int)
+	resultChannel := make(chan int)
+	done := make(chan struct{})
+
+	var wg sync.WaitGroup
+
+	go producer(workChannel)
+
+	// can vary the number of workers
+	for i := 0; i < 200; i++ {
+		wg.Add(1)
+		go worker(workChannel, resultChannel, &wg)
+	}
+
+	go func() {
+		wg.Wait()
+		close(done)
+	}()
+
+	counter := 0
+
+	for {
+		select {
+		case i := <-resultChannel: // these race
+			counter++
+			fmt.Println(i)
+		case <-done:
+			fmt.Println("Done")
+			goto loop
+		}
+	}
+loop:
+	fmt.Println("Processed ", counter, " items")
+}
